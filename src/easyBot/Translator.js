5c3467ff9328568fdf2632ccad4e3ed70b8bc70b
racer242 <racer242@gmail.com>
2022-04-22 10:59:17 +0300

    translator tests added

--- src/easyBot/Translator.js
+++ src/easyBot/Translator.js
@@ -1,29 +1,50 @@
 import translate from "@iamtraction/google-translate";
 
 class Translator {
+  /**
+   * Source language like "en", "fr", "ru"
+   * @type {String}
+   */
   langFrom;
 
+  /**
+   * Terget language like "en", "fr", "ru"
+   * @type {String}
+   */
   langTo;
 
+  /**
+   * Class constructor
+   * @param {String} langFrom=null  Source language
+   * @param {String} langTo="en"    Terget language
+   */
   constructor(langFrom = null, langTo = "en") {
     this.langFrom = langFrom;
     this.langTo = langTo;
   }
 
+  /**
+   * Start translation
+   * @param  {String}  phrase               Source phrase to translate
+   * @return {Promise}        Translated phrase
+   */
   async run(phrase) {
     let translation = null;
-    let { langFrom } = this;
+    let langCorrected = null;
+    let textCorrected = null;
     try {
+      let { langFrom } = this;
       let result = await translate(phrase, {
         from: langFrom,
         to: this.langTo,
       });
       translation = result.text;
-      let langCorrected = null;
-      let textCorrected = null;
 
       if (result?.from?.language?.didYouMean) {
         langCorrected = result.from.language.iso;
+      }
+      if (result?.from?.text?.didYouMean) {
+        textCorrected = result.from.text.value;
       }
 
       if (!langFrom && langCorrected) {
@@ -34,25 +55,31 @@ class Translator {
         });
         translation = result.text;
       }
-      if (result.from.text.didYouMean) {
+      if (!textCorrected && result?.from?.text?.didYouMean) {
         textCorrected = result.from.text.value;
       }
-
-      return {
-        translation,
-        langCorrected,
-        textCorrected,
-      };
     } catch (e) {
       console.log(e);
     }
-    return translation;
+    return {
+      translation,
+      langCorrected,
+      textCorrected,
+    };
   }
 
+  /**
+   * Set target language
+   * @param {String} langTo  Target language
+   */
   setTo(langTo) {
     this.langTo = langTo;
   }
 
+  /**
+   * Set source language
+   * @param {String} langTo  Source language
+   */
   setFrom(langFrom) {
     this.langFrom = langFrom;
   }
--- src/tests/unit/translator.test.js
+++ src/tests/unit/translator.test.js
@@ -1,9 +1,106 @@
-import { translate } from "../../easybot/translator";
+import translate from "@iamtraction/google-translate";
+import Translator from "../../easybot/Translator";
 
-beforeEach(() => {});
+jest.mock("@iamtraction/google-translate");
 
-describe("Translate func", () => {
-  test("should be true", () => {
-    expect(translate()).toBe(true);
+describe("Translate function run", () => {
+  let translator;
+  let phrase;
+  let result;
+  let resp;
+
+  beforeEach(() => {
+    translator = new Translator();
+  });
+
+  let testFunc = async () => {
+    translate.mockImplementation(() => {
+      return Promise.resolve(resp);
+    });
+
+    let translation = await translator.run(phrase);
+    return translation;
+  };
+
+  test("should translate correct phrase", async () => {
+    phrase = "Проверка";
+    result = "Check";
+    resp = { text: result };
+    const { translation, langCorrected, textCorrected } = await testFunc();
+    expect(translation).toBe(result);
+    expect(langCorrected).toBeNull();
+    expect(textCorrected).toBeNull();
+  });
+
+  test("should translate incorrect phrase and correct text", async () => {
+    phrase = "Проврка";
+    result = "Check";
+    resp = {
+      text: result,
+      from: {
+        language: { didYouMean: false, iso: "" },
+        text: { didYouMean: true, value: "Check" },
+      },
+    };
+    const { translation, langCorrected, textCorrected } = await testFunc();
+    expect(translation).toBe(result);
+    expect(langCorrected).toBeNull();
+    expect(textCorrected).not.toBeNull();
+  });
+
+  test("should translate incorrect phrase and change lang", async () => {
+    phrase = "Проврка";
+    result = "Check";
+    resp = {
+      text: result,
+      from: {
+        language: { didYouMean: true, iso: "sr" },
+        text: { didYouMean: false, value: "" },
+      },
+    };
+    const { translation, langCorrected, textCorrected } = await testFunc();
+    expect(translation).toBe(result);
+    expect(langCorrected).not.toBeNull();
+    expect(textCorrected).toBeNull();
+  });
+
+  test("should return nulls if empty response", async () => {
+    phrase = "Проверка";
+    resp = null;
+    const { translation, langCorrected, textCorrected } = await testFunc();
+    expect(translation).toBeNull();
+    expect(langCorrected).toBeNull();
+    expect(textCorrected).toBeNull();
+  });
+
+  test("should return nulls if exception", async () => {
+    phrase = "Проверка";
+    translate.mockImplementation(() => {
+      throw new Error("Some error occured!");
+    });
+
+    const { translation, langCorrected, textCorrected } = await translator.run(
+      phrase
+    );
+    expect(translation).toBeNull();
+    expect(langCorrected).toBeNull();
+    expect(textCorrected).toBeNull();
+  });
+});
+
+describe("Setup functions", () => {
+  let translator;
+  let lang;
+  beforeEach(() => {
+    translator = new Translator();
+    lang = "ru";
+  });
+  test("should set target language", async () => {
+    translator.setTo(lang);
+    expect(translator.langTo).toEqual(lang);
+  });
+  test("should set source language", async () => {
+    translator.setFrom(lang);
+    expect(translator.langFrom).toEqual(lang);
   });
 });
